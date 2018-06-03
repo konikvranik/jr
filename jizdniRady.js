9c46934c861bde81bafd9ec0f0a5089588db99d7
Petr Vranik <petr@vranik.name>
2018-06-03 15:08:16 +0200

    script

---
+++ jizdniRady.js
@@ -0,0 +1,123 @@
+var https = require("https");
+var dateFormat = require("dateformat");
+
+var now = new Date();
+var start = new Date(now - now.getDay() + 8 * 86400000);
+
+function getUri(from, to, date) {
+  return (
+    "https://ext.crws.cz/api/ABCz/departures?from=" +
+    encodeURIComponent(from) +
+    "&to=" +
+    encodeURIComponent(to) +
+    "&maxCount=0&dateTime=" +
+    date.toISOString()
+  );
+}
+
+function formatOutput(parsedData) {
+  var last = "";
+  return parsedData.trains
+    .map(
+      train =>
+        (t => {
+          if (t == last) {
+            return "";
+          } else {
+            last = t;
+            return "\n\n###" + t + "\n\n";
+          }
+        })(train.timeTrainStart.split(":")[0]) +
+        "**" +
+        train.timeTrainStart +
+        "** _(" +
+        [train.train.num1]
+          .concat(
+            train.train.fixedCodes
+              ? train.train.fixedCodes.map(c => c.text)
+              : []
+          )
+          .join(",") +
+        ")_"
+    )
+    .join(", ");
+}
+
+var buffer;
+var endDay;
+var startDay;
+
+function doIt(from, to, date) {
+  if (!endDay) endDay = dateFormat(date, "ddd");
+  if (!startDay) startDay = dateFormat(date, "ddd");
+
+  https: https
+    .get(getUri(from, to, date), res => {
+      const { statusCode } = res;
+      const contentType = res.headers["content-type"];
+
+      let error;
+      if (statusCode !== 200) {
+        error = new Error("Request Failed.\n" + `Status Code: ${statusCode}`);
+      } else if (!/^application\/json/.test(contentType)) {
+        error = new Error(
+          "Invalid content-type.\n" +
+            `Expected application/json but received ${contentType}`
+        );
+      }
+      if (error) {
+        console.error(res);
+        console.error(error.message);
+        // consume response data to free up memory
+        res.resume();
+        process.exit(1);
+      }
+
+      res.setEncoding("utf8");
+      let rawData = "";
+      res.on("data", chunk => {
+        rawData += chunk;
+      });
+      res.on("end", () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          var output = formatOutput(parsedData);
+          if (!buffer) {
+            buffer = output;
+          } else if (date.getDay() == 0 || date.getDay() > 6) {
+            process.stdout.write(
+              "\n\n## " +
+                startDay +
+                " - " +
+                dateFormat(date, "ddd") +
+                "\n" +
+                buffer
+            );
+            return;
+          } else if (buffer == output) {
+            endDay = dateFormat(date, "ddd");
+          } else {
+            buffer = output;
+            process.stdout.write(
+              "\n\n## " + startDay + " - " + endDay + "\n" + buffer
+            );
+            startDay = dateFormat(date, "ddd");
+            endDay = null;
+          }
+          doIt(from, to, new Date(date.getTime() + 86400000));
+        } catch (e) {
+          console.error(e.message);
+          process.exit(1);
+        }
+      });
+    })
+    .on("error", e => {
+      console.error(`Got error: ${e.message}`);
+      process.exit(1);
+    });
+}
+
+process.stdout.write(
+  "\n\n#" + process.argv[2] + " -> " + process.argv[3] + "\n\n"
+);
+doIt(process.argv[2], process.argv[3], start);
